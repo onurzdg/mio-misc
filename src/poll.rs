6874f4bbd997ecde45d0905ddd6344371fd0eba9
daxpedda <daxpedda@gmail.com>
2021-03-07 00:27:23 +0100

    Return mutable `Event`

--- src/poll.rs
+++ src/poll.rs
@@ -20,12 +20,12 @@ impl Poll {
     }
 
     /// Polls until the provided duration and returns `Events`
-    pub fn wait<I>(&mut self, timeout: I) -> io::Result<&mio::Events>
+    pub fn wait<I>(&mut self, timeout: I) -> io::Result<&mut mio::Events>
     where
         I: Into<Option<Duration>>,
     {
         let _ = self.poll.poll(&mut self.events, timeout.into())?;
-        Ok(&self.events)
+        Ok(&mut self.events)
     }
 
     /// Returns registry
